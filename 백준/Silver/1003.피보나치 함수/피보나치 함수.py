aa5eebb106b191f13e96f028f36c70862a709c70
Junser Hwang <76620338+hrabit64@users.noreply.github.com>
2022-02-01 20:45:48 +0900

    Update 피보나치 함수.py

--- 백준/Silver/1003.피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003.피보나치 함수/피보나치 함수.py
@@ -2,14 +2,15 @@ import sys
 n = int(sys.stdin.readline().strip())
 nums = [int(sys.stdin.readline().strip()) for _ in range(n)]
 
-fibo_out_zero = [1,0,1,1]
-fibo_out_one = [0,1,1,2]
+fibo_out = [(1,0),(0,1),(1,1),(1,2)]
 for num in nums:
 
-    if num <= len(fibo_out_zero)-1:
-        sys.stdout.write(str(fibo_out_zero[num])+" "+str(fibo_out_one[num])+"\n")
+    if num <= len(fibo_out)-1:
+        sys.stdout.write(str(fibo_out[num][0])+" "+str(fibo_out[num][1])+"\n")
     else:
-        for i in range(len(fibo_out_zero),num+1):
-            fibo_out_zero.append(fibo_out_zero[i-1]+fibo_out_zero[i-2])
-            fibo_out_one.append(fibo_out_one[i - 1] + fibo_out_one[i - 2])
-        sys.stdout.write(str(fibo_out_zero[num])+" "+str(fibo_out_one[num])+"\n")
+        for i in range(len(fibo_out),num+1):
+            fibo_out.append((fibo_out[i-1][0]+fibo_out[i-2][0],fibo_out[i-1][1]+fibo_out[i-2][1]))
+        sys.stdout.write(str(fibo_out[num][0])+" "+str(fibo_out[num][1])+"\n")
+
+
+
